d9fcfb487bdbfe4c19f6e791f3f4693a5080b538
Thomas <viciousbadger@protonmail.com>
2017-08-07 14:40:11 +0200

    Seperated more buildings, building circles, text mockup

---
+++ Assets/MaterialSlide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class MaterialSlide : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 velocity;
+
+    private void Update()
+    {
+        GetComponent<MeshRenderer>().material.mainTextureOffset += velocity * Time.deltaTime;
+    }
+}
