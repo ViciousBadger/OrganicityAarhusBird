71e5414487b750a282ee4a40574785e3ee193a66
Thomas <viciousbadger@protonmail.com>
2017-08-08 11:14:39 +0200

    Post processing, more UI mockups

---
+++ Assets/CanvasGroupDistanceFade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasGroup), typeof(RectTransform))]
+public class CanvasGroupDistanceFade : MonoBehaviour
+{
+    [SerializeField]
+    private float visibleDistance;
+    [SerializeField]
+    private float invisibleDistance;
+
+    private CanvasGroup cg;
+    private RectTransform rt;
+
+    private void Start()
+    {
+        cg = GetComponent<CanvasGroup>();
+        rt = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        float visibility = Mathf.InverseLerp(invisibleDistance, visibleDistance, rt.anchoredPosition.x);
+        cg.alpha = visibility;
+    }
+}
